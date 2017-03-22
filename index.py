95f4872b173c81f832535b8cb8b427120851c9c1
kugan49 <kugan49@gmail.com>
2017-03-22 23:45:44 +0100

    Update index.py

--- index.py
+++ index.py
@@ -17,10 +17,10 @@ def orgaFic(pathFiles):
     return render_template('orgaFic.html',pathFiles=pathFiles,path='/')
 @app.route('/dev/')
 def dev():
-    url_root=request.url_root
+    url_root=request.url_root.lstrip
     ind=url_root.find(':',6)
     if (ind != -1) : url_root = url_root[: ind]
-    url_root.replace('http://','https://')
+    url_root=url_root.replace('http://','https://')
     if (url_root.endswith('/') == False) : url_root+='/'
     return render_template('dev.html',path=url_root)
 @app.route('/FolderBuilder/')
