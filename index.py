c9a07dff8e6f6636607f7af51da345158e01d192
kugan49 <kugan49@gmail.com>
2017-03-23 23:27:58 +0100

    Update index.py

--- index.py
+++ index.py
@@ -34,8 +34,8 @@ def majWeb():
 #                                         En cours développement
 @app.route('/test/')
 def test():
-    import filetree/flask_filetree
-    filetree/flask_filetree.test()
+    import filetree
+    filetree.test()
     return 'Test OK !!!' + request.url_root + '<br>' + request.base_url
 @app.route('/orgaFic/<path:pathFiles>')
 def orgaFic(pathFiles):
