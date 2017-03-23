5f4476eb0ba70e15e086e92a3b761ec495eb137e
kugan49 <kugan49@gmail.com>
2017-03-23 08:54:18 +0100

    Update index.py

--- index.py
+++ index.py
@@ -5,16 +5,10 @@ from flask import Flask, flash, render_template, redirect, request, url_for
 app = Flask(__name__)  
 app.debug = True
 app.secret_key = 'kugan49'
-
+#OK Terminé
 @app.route('/')
 def index():
     return render_template('index.html')
-@app.route('/test/')
-def test():
-    return 'Test OK !!!' + request.url_root + '<br>' + request.base_url
-@app.route('/orgaFic/<path:pathFiles>')
-def orgaFic(pathFiles):
-    return render_template('orgaFic.html',pathFiles=pathFiles,path='/')
 @app.route('/dev/')
 def dev():
     url_root=request.url_root.lstrip()
@@ -26,11 +20,6 @@ def dev():
 @app.route('/JD/')
 def JD():
   return render_template('jDownloader.html',path='https://my.jdownloader.org/?deviceId=f85d6762b1f540de196fa733c649891b#webinterface:downloads')
-  
-@app.route('/FolderBuilder/')
-def FolderBuilder():
-    return render_template('/FolderBuilder/index.html')
-  
 @app.route('/majWeb/')
 def majWeb():
     #import subprocess
@@ -39,6 +28,22 @@ def majWeb():
     os.system('sh /home/osmc/scripts/majWeb.sh')
     flash('OK<br>maj effectuée !!!!')
     return redirect(url_for('index'))
+
+  
+  
+#                                         En cours développement
+@app.route('/test/')
+def test():
+    return 'Test OK !!!' + request.url_root + '<br>' + request.base_url
+@app.route('/orgaFic/<path:pathFiles>')
+def orgaFic(pathFiles):
+    return render_template('DDL.html',pathFiles=pathFiles,path='/')
+    #return render_template('orgaFic.html',pathFiles=pathFiles,path='/')
+
+@app.route('/FolderBuilder/')
+def FolderBuilder():
+    return render_template('/FolderBuilder/index.html')
+  
     
   
 if __name__ == '__main__':
