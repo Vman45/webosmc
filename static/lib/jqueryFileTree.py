00fcbda1605a29987edcb90ecca4da17f033117a
kugan49 <kugan49@gmail.com>
2017-03-23 22:10:45 +0100

    Update jqueryFileTree.py

--- static/lib/jqueryFileTree.py
+++ static/lib/jqueryFileTree.py
@@ -10,7 +10,7 @@ def dirlist(request):
    r=['<ul class="jqueryFileTree" style="display: none;">']
    try:
        r=['<ul class="jqueryFileTree" style="display: none;">']
-       d=urllib.unquote(request.POST.get('dir','c:\\temp'))
+       d=urllib.unquote(request.POST.get('dir','mnt/USB/LOC/DDL_OK/'))
        for f in os.listdir(d):
            ff=os.path.join(d,f)
            if os.path.isdir(ff):
