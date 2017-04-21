4f0f3292de6986422964c0416d9ee227225055e4
kugan49 <kugan49@gmail.com>
2017-04-21 09:33:41 +0200

    Update test_accueil.py

--- test/test_accueil.py
+++ test/test_accueil.py
@@ -9,29 +9,29 @@ class MyTestClass(unittest.TestCase):
     # code that is executed before all tests in one test run
     @classmethod
     def setUpClass(cls):
-       pass 
+        pass 
 
-    # clean up logic for the test suite declared in the test module
-    # code that is executed after all tests in one test run
+        # clean up logic for the test suite declared in the test module
+        # code that is executed after all tests in one test run
     @classmethod
     def tearDownClass(cls):
-       pass 
+        pass 
 
-    # initialization logic
-    # code that is executed before each test
+        # initialization logic
+        # code that is executed before each test
     def setUp(self):
-    # creates a test client
-    self.app = app.test_client()
-    # propagate the exceptions to the test client
-    self.app.testing = True 
+        # creates a test client
+        self.app = app.test_client()
+        # propagate the exceptions to the test client
+        self.app.testing = True 
 
 
-    # clean up logic
-    # code that is executed after each test
+        # clean up logic
+        # code that is executed after each test
     def tearDown(self):
-    pass 
+        pass 
 
-    # test method
+        # test method
     def test_home_status_code(self):
         # sends HTTP GET request to the application
         # on the specified path
@@ -47,13 +47,13 @@ class MyTestClass(unittest.TestCase):
 
         # assert the response data
         self.assertEqual(result.data, "Hello World!!!")
-  
-    def test_equal_numbers(self):
-    self.assertEqual(2, 2) 
 
-# runs the unit tests in the module
-if __name__ == '__main__':
-    unittest.main()
+    def test_equal_numbers(self):
+        self.assertEqual(2, 2) 
+
+    # runs the unit tests in the module
+    if __name__ == '__main__':
+        unittest.main()
 
 
 
