39ab8d43a8bd5fcbd8645309f1f2e369fb79456e
kugan49 <kugan49@gmail.com>
2017-05-03 14:53:38 +0200

    Vérif version

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ GEN_MENU[6] = {"Config":"/config/"}
 GEN_MENU[7] = {"test":"/test/"}
 GEN_AFF_BANDEAU = False
 LINK_MAJ_SITE = "git --git-dir /home/osmc/webosmc/.git pull" #"sh /home/osmc/scripts/majWeb.sh"
-LINK_VERIFMAJ = ["sh","/home/osmc/webosmc/verifVersion.sh"]
+LINK_VERIFMAJ = ["sh","/home/osmc/webosmc/verifVersion.sh"]#["git","-C /home/osmc/webosmc/ pull --dry-run 2>&1 | grep 'master' >/dev/null |echo $?"]
 LINK_JDOWNLOADER = "https://my.jdownloader.org/?deviceId=4ad32ae65d600a9b070bba54a8f0429d#webinterface:downloads"
 GESTIONFICHIER_DDL_PATH = "/mnt/LOC/USB"
 GESTIONFICHIER_LST_EXCL_PATH = ['$RECYCLE.BIN','save', 'System Volume Information', 'test', 'musique', 'photos']
