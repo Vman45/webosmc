7948474eae3eb0e744e4ebb699defc9824b8bfe2
kugan49 <kugan49@gmail.com>
2017-06-07 23:19:58 +0200

    Mise en place wymypy --> MPD

--- modules/MPD/app.py
+++ modules/MPD/app.py
@@ -1,19 +1,104 @@
+from flask import Flask, render_template, request, abort, Response, Blueprint
+MPDClient = Blueprint('MPDClient',__name__, template_folder='templates')
+app = Flask(__name__)
+app.config.from_object('config')
+
+
+
 import ConfigParser
 from functools import wraps
 import json
-
-from flask import Flask, render_template, request, abort, Response, Blueprint
-app = Flask(__name__)
-MPDClient = Blueprint('MPDClient',__name__, template_folder='templates')
 from modules.MPD.libs.mpdsafe import MpdSafe
 
 config = ConfigParser.SafeConfigParser()
 mpd = MpdSafe()
-plugins = []
+plugins = {}
+InitDone = False
 
+import inspect
+import logging
+import os
+import os.path
+import sys
+
+
+def init():
+    config_file=app.config["MPD_CONFIG"]
+    config.read(config_file)
+    plugin_configs = {}
+    for section in config.sections():
+        if not section.startswith("plugin-"): continue
+        plugin_configs[section[7:]] = dict([(option, config.get(section, option)) for option in config.options(section)])
+
+    # mpd->stream is special as it implies plugins-player->has_stream
+    plugin_configs['player']['has_stream'] = bool(config.get('mpd', 'stream'))
+
+    # connect to MPD
+    mpd.connect(config.get("mpd", "host"), config.getint("mpd", "port"))
+
+    # discover plugins
+    global plugins
+    plugins = get_instances(mpd, config.get("plugins", "banned").split(","), plugin_configs)
+    app.logger.debug("val plugins dans init " + str(plugins))
+    InitDone = True
+   
+def get_instances(mpd, bannend_plugins=[], plugin_configs={}):
+    """ instantiate classes which inherits of me, return the list """
+    instances = {}
+    base_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "plugins")
+
+    for plugin_file_name in os.listdir(base_dir):
+        # filter out directories
+        if not os.path.isfile(os.path.join(base_dir, plugin_file_name)): continue
+
+        # filter out non py files
+        if not plugin_file_name.endswith(".py"): continue
+
+        # filter out special modules
+        if plugin_file_name in ("__init__.py", ): continue
+
+        # determine actual module name
+        plugin_module_name = os.path.splitext(plugin_file_name)[0]
+
+        # check for ban
+        if plugin_module_name in bannend_plugins: continue
+
+        # load it
+        try:
+            __import__("modules.MPD.plugins.%s" % plugin_module_name)
+        except Exception, m:
+            app.logger.exception("Plugin import error for [%(name)s]: %(error)s" % {'name': plugin_module_name, 'error': m})
+            continue
+
+        # lookup module
+        plugin_module = sys.modules["modules.MPD.plugins.%s" % plugin_module_name]
+
+        # lookup class
+        if not hasattr(plugin_module, plugin_module_name.capitalize()): continue
+        plugin_class = getattr(plugin_module, plugin_module_name.capitalize())
+
+        # make sure its the right one
+        if not inspect.isclass(plugin_class):
+            continue
+
+        app.logger.info(" - load plugin: %s" % plugin_module_name.capitalize())
+        try:
+            instances[plugin_module_name] = plugin_class(mpd, plugin_configs.get(plugin_module_name, {}))
+            instances[plugin_module_name].name = plugin_module_name
+        except Exception, m:
+            app.logger.error("Plugin instantiation error for %s: %s" % (plugin_module_name, m))
+    app.logger.debug("return plugins " + str(instances))
+    return instances
+
+                       
+                       
+                       
+                       
 def requires_auth(f):
     @wraps(f)
     def decorated(*args, **kwargs):
+        if InitDone == False:
+            init()
         if config.has_option("auth", "enabled") and config.getboolean("auth", "enabled"):
             auth = request.authorization
             if not auth or not (auth.username == config.get("auth", "username") and auth.password == config.get("auth", "password")):
@@ -22,9 +107,10 @@ def requires_auth(f):
     return decorated
 
 
-@MPDClient.route('/MPD')
+@MPDClient.route('/MPD/')
 @requires_auth
 def root():
+    app.logger.debug("val plugins dans root " + str(plugins))
     return render_template('MPD_main.html',
                            player=plugins['player'].index(),
                            player_playlist=plugins['player'].playlist(),
@@ -73,32 +159,3 @@ def ajax_methods(method, plugin=None):
         return response
 
     abort(404)
-
-def main():
-    config_file=app.config["MPD_CONFIG"]
-    wymypy.app.config.read(config_file)
-    plugin_configs = {}
-    for section in wymypy.app.config.sections():
-        if not section.startswith("plugin-"): continue
-        plugin_configs[section[7:]] = dict([(option, wymypy.app.config.get(section, option)) for option in wymypy.app.config.options(section)])
-
-    # mpd->stream is special as it implies plugins-player->has_stream
-    plugin_configs['player']['has_stream'] = bool(wymypy.app.config.get('mpd', 'stream'))
-
-    # configure logging
-    if wymypy.app.config.has_option("general", "logging"):
-        logging.basicConfig(filename=wymypy.app.config.get("general", "logging"), format="%(asctime)s %(levelname)s %(message)s", level=logging.DEBUG)
-    else:
-        logging.basicConfig(format="%(asctime)s %(levelname)s %(message)s", level=logging.DEBUG)
-
-    # connect to MPD
-    wymypy.app.mpd.connect(wymypy.app.config.get("mpd", "host"), wymypy.app.config.getint("mpd", "port"))
-
-    # discover plugins
-    wymypy.app.plugins = get_instances(wymypy.app.mpd, wymypy.app.config.get("plugins", "banned").split(","), plugin_configs)
-
-    # start server
-    wymypy.app.app.run(host=wymypy.app.config.get("server", "interface"),
-                       port=wymypy.app.config.getint("server", "port"),
-                       debug=wymypy.app.config.getboolean("general", "debug"),
-                       request_handler=WyMyPyRequestHandler)
