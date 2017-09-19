4a228288f3ebe89dfe57baa8d515853624c36f48
kugan49 <kugan49@gmail.com>
2017-09-19 16:58:33 +0200

    Ajout du système Cloud : Permet de télécharger et uploader des fichiers présents sur le Raspberry

--- modules/ThrowBox/app.py
+++ modules/ThrowBox/app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request, abort, Response, Blueprint
-ThrowBox = Blueprint('ThrowBox',__name__, static_folder='static', static_url_path='/modules/ThrowBox/static', template_folder='/modules/ThrowBox/templates')
+# coding: utf-8
+from flask import Flask, render_template, request, abort, Response, Blueprint, send_file
+ThrowBox = Blueprint('ThrowBox',__name__, static_folder='static', static_url_path='/modules/ThrowBox/static', template_folder='templates')
 app = Flask(__name__)
 app.config.from_object('config')
 
@@ -7,74 +8,98 @@ app.config.from_object('config')
 import ConfigParser
 from functools import wraps
 config = ConfigParser.SafeConfigParser()
+import base64
 
 InitDone = False
+StockPath = ""
 
 import os
 import time
 import functions
+import platform
+from werkzeug import secure_filename
+
+@ThrowBox.route('/upload')
+def upload_file():
+   return render_template('upload.html')
+	
 
 def init():
-    config_file=app.config["THROWBOX_CONFIG"]
+    if platform.system()[:3] == 'Win':
+        LOCAL_PATH=os.path.dirname(os.path.realpath(__file__))
+        config_file=os.path.join(LOCAL_PATH, "throwbox.ini")
+    else:
+        config_file=app.config["THROWBOX_CONFIG"]
     config.read(config_file)
-    plugin_configs = {}
-    for section in config.sections():
-        plugin_configs[section] = dict([(option, config.get(section, option)) for option in config.options(section)])
+    global StockPath
+    StockPath=config.get("REPO","path").decode('utf-8',errors='ignore')
     InitDone = True
     
-                       
+
 def requires_auth(f):
     @wraps(f)
     def decorated(*args, **kwargs):
         if InitDone == False:
             init()
-        if config.has_option("AUTH", "Enabled") and config.getboolean("AUTH", "Enabled"):
+        if config.has_option("AUTH", "enabled") and config.getboolean("AUTH", "enabled"):
+            app.logger.info('Asking authorization')
             auth = request.authorization
-            if not auth or not (auth.username == config.get("AUTH", "userName") and auth.password == config.get("AUTH", "Password")):
-                return Response('Could not verify your access level for that URL.\nYou have to login with proper credentials', 401, {'WWW-Authenticate': 'Basic realm="Login Required"'})
+            if not auth or not (auth.username == config.get("AUTH", "username") and auth.password ==  base64.b64decode(config.get("AUTH", "password"))):
+                return Response(u"Autorisation acces refuse\nVous devez vous authentifier correctement", 401, {'WWW-Authenticate': 'Basic realm="Login Required"'})
         return f(*args, **kwargs)
     return decorated
     
-@app.route("/")
+@ThrowBox.route("/")
 @requires_auth
 def index():
-	return flask.render_template("index.html")
+    return render_template("throwbox.html")
 
-@app.route("/browse/")
+@ThrowBox.route("/browse/")
 @requires_auth
 def root():
-	BASE_PATH=""
-	folders,files=functions.ReadPath("")
-	#route=["/"]
-	print(files)
-	return flask.render_template("root.html",**locals())
+    BASE_PATH=""
+    # app.logger.info('StockPath : ' + StockPath)
+    folders,files=functions.ReadPath(StockPath)
+    print(files)
+    return render_template("root.html",**locals())
 
-@app.route("/browse/<path:path>")
+@ThrowBox.route("/browse/<path:path>", methods = ['GET', 'POST'])
 @requires_auth
 def browse(path):
-	BASE_PATH=path
+    BASE_PATH=path
+    path=os.path.join(StockPath,path)
+    
+    if request.method == 'POST':
+        f = request.files['file']
+        filename = secure_filename(f.filename)
+        f.save(os.path.join(path, filename))
+        return 'file uploaded successfully'
+    else:
+        if functions.IsFile(path):
+            return send_file(path)
+        # 404
+        elif functions.ReadPath(path)==None:
+            functions.log("Error; 404; IP=%s" % request.remote_addr)
+            Error404=True
+            ClientIP=request.remote_addr
+            return render_template("browse.html",**locals())
+        else:
+            folders,files=functions.ReadPath(path)
+            # app.logger.info("folders: %s  \n files : %s" %(folders,files))
+            # Remove the hidden files and folders (hidden=beggining with ".")
+            folders=functions.RemoveHiddenObjects(folders)
+            files=functions.RemoveHiddenObjects(files)
 
-	# 404
-	if functions.ReadPath(path)==None:
-		functions.log("Error; 404; IP=%s" % flask.request.remote_addr)
-		Error404=True
-		ClientIP=flask.request.remote_addr
-		return flask.render_template("browse.html",**locals())
-	
-	elif functions.IsFile(path):
-		return flask.send_file(functions.RepoPath(path))
-		
-	else:
-		folders,files=functions.ReadPath(path)
-
-		# Remove the hidden files and folders (hidden=beggining with ".")
-		folders=functions.RemoveHiddenObjects(folders)
-		files=functions.RemoveHiddenObjects(files)
-
-		# Sort the folders and files list.
-		folders.sort()
-		files.sort()
-
-	route=path.split("/")
-
-	return flask.render_template("browse.html",**locals())
+            # Sort the folders and files list.
+            folders.sort()
+            files.sort()
+        tmproute=path.split("/")
+        route=[]
+        u="/ThrowBox/browse"
+        for r in tmproute:
+            if r not in StockPath:
+                u=u + '/' + r
+                route.append(dict(name=r, url=u))        
+            
+        # app.logger.info("route: %s" %route)
+        return render_template("browse.html",**locals())
--- modules/ThrowBox/functions.py
+++ modules/ThrowBox/functions.py
@@ -1,60 +1,73 @@
+# coding: utf-8
+from __future__ import unicode_literals
+from flask import Flask
 import os
 import time
+app = Flask(__name__)
+
 
 def ReadPath(path):
-	LOCAL_PATH=os.path.dirname(os.path.realpath(__file__))
-	REPO=os.path.join(LOCAL_PATH, "REPO")
-
-
-	REQUESTED_PATH=os.path.join(REPO,path)
-
-	if not os.path.exists(REQUESTED_PATH):
-		print("Doesnt exist.")
-		return None
-
-	try:
-		stuff=next(os.walk(REQUESTED_PATH))
-		folders=stuff[1]
-		files=stuff[2]
-		return folders,files
-	except StopIteration:
-		print("empty.")
-		return 0
+    try:
+        exists = os.path.exists(path)
+    except UnicodeDecodeError:
+        exists = os.path.exists(path.decode('utf-8'))
+        path = path.decode('utf-8')
+    except UnicodeEncodeError:
+        exists = os.path.exists(path.encode('utf-8'))
+        path = path.encode('utf-8')
+    if not exists:
+        print("Doesnt exist.")
+        return None
+    try:
+        files=[]
+        folders=[]
+        lst = os.listdir(path)
+        for name in lst:
+            try:
+                fn = os.path.join(path, name)
+            except UnicodeDecodeError:
+                fn = os.path.join(path.encode('utf-8'), name)
+            try:
+                isDir = os.path.isdir(fn)
+            except UnicodeEncodeError:
+                isDir = os.path.isdir(fn.encode('utf-8'))
+            if isDir == False:
+                files.append(name.decode('utf-8',errors='ignore')) 
+            else:
+                folders.append(name.decode('utf-8',errors='ignore'))
+        return folders,files
+    except StopIteration:
+        print("empty.")
+        return 0
 
 def IsFile(path):
-	LOCAL_PATH=os.path.dirname(os.path.realpath(__file__))
-	REPO=os.path.join(LOCAL_PATH, "REPO")
-	FINAL_PATH=os.path.join(REPO,path)
-
-	if os.path.isdir(FINAL_PATH):
-		return False
-	else:
-		return True
-
-def RepoPath(path):
-	LOCAL_PATH=os.path.dirname(os.path.realpath(__file__))
-	REPO=os.path.join(LOCAL_PATH, "REPO")
-	FINAL_PATH=os.path.join(REPO,path)
-
-	return FINAL_PATH
-
+    try:
+        exists = os.path.isdir(path)
+    except UnicodeDecodeError:
+        exists = os.path.isdir(path.decode('utf-8'))
+    except UnicodeEncodeError:
+        exists = os.path.isdir(path.encode('utf-8'))
+    if exists:
+        return False
+    else:
+        return True
 
 def RemoveHiddenObjects(_list):
-	_list2=list() # List that contains only visible,
-				  # non-hidden objects.
-	for element in _list:
-		if element[0]!=".":
-			_list2.append(element)
+    _list2=list() # List that contains only visible,
+                  # non-hidden objects.
+    for element in _list:
+        if element[0]!=".":
+            _list2.append(element)
 
-	return _list2
+    return _list2
 
 
 def log(message, file="log.txt"):
-	"""A barebones function that logs messages."""
-	line="[{} >>> {}]\n\t{}\n\n".format(time.strftime("%d/%m/%Y"), time.strftime("%H:%M:%S"),message)
-	file=open(file, "a")
-	file.write(line)
-	file.close()
+    """A barebones function that logs messages."""
+    line="[{} >>> {}]\n\t{}\n\n".format(time.strftime("%d/%m/%Y"), time.strftime("%H:%M:%S"),message)
+    file=open(file, "a")
+    file.write(line)
+    file.close()
 
 
 
