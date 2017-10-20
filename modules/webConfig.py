76988cd568083ddf0be6251024f680790a504d65
kugan49 <kugan49@gmail.com>
2017-10-20 09:45:48 +0200

    execution multiple

--- modules/webConfig.py
+++ modules/webConfig.py
@@ -25,8 +25,55 @@ def modifPortURL(URL,Port):
     return URL
    
 def launch_process(param):
-    process = subprocess.Popen(param, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    process.wait()
+    inc=0
+    cmd=[]
+    for word in param:
+        cmd[inc].append(word)
+        if "|" in word:
+            inc=inc+1
+    if inc = 0:    
+        process = subprocess.Popen(param, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        process.wait()
+    else:
+        process.stdout = ""
+        for c in cmd:
+            process = subprocess.Popen(param, stdin=process.stdout,stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            process.wait()
+        # Connecting Segments of a Pipe
+
+        # Multiple commands can be connected into a pipeline, similar to the way the Unix shell works, by creating separate Popen instances and chaining their inputs and outputs together. The stdout attribute of one Popen instance is used as the stdin argument for the next in the pipeline, instead of the constant PIPE. The output is read from the stdout handle for the final command in the pipeline.
+
+        # subprocess_pipes.py
+        # import subprocess
+
+        # cat = subprocess.Popen(
+            # ['cat', 'index.rst'],
+            # stdout=subprocess.PIPE,
+        # )
+
+        # grep = subprocess.Popen(
+            # ['grep', '.. literalinclude::'],
+            # stdin=cat.stdout,
+            # stdout=subprocess.PIPE,
+        # )
+
+        # cut = subprocess.Popen(
+            # ['cut', '-f', '3', '-d:'],
+            # stdin=grep.stdout,
+            # stdout=subprocess.PIPE,
+        # )
+
+        # end_of_pipe = cut.stdout
+
+        # print('Included files:')
+        # for line in end_of_pipe:
+            # print(line.decode('utf-8').strip())
+        # The example reproduces the command line:
+
+        # $ cat index.rst | grep ".. literalinclude" | cut -f 3 -d:
+        # The pipeline reads the reStructuredText source file for this section and finds all of the lines that include other files, then prints the names of the files being included.
+        
+        
     return {'output' : unicode(process.stdout.read(),"utf-8"), 'error' : unicode(process.stderr.read(),"utf-8")}
   
 
