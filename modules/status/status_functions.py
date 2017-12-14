6abbf77c8562d3e2e60a72c88726bd99df45df93
kugan49 <kugan49@gmail.com>
2017-12-14 21:15:57 +0100

    Correction sur Erreur Recherche IP

--- modules/status/status_functions.py
+++ modules/status/status_functions.py
@@ -87,7 +87,11 @@ def infoNetwork():
 
     for name in list(Tab.keys()):
         p = Tab[name]
-        IP =  ifaddresses(name)[AF_INET][0]['addr']
+        try:
+            IP =  ifaddresses(name)[AF_INET][0]['addr']
+        except:
+            IP = ""
+            pass
         TabRet[name] = {'ip' : IP, 'bytes_sent' : getDisplayValue(p.bytes_sent),'bytes_recv':getDisplayValue(p.bytes_recv),'bytes_sent_raw' : p.bytes_sent,'bytes_recv_raw': p.bytes_recv}        
     return TabRet
     
