00d75010315a4eebb0c0a257510be686562294ff
kugan49 <kugan49@gmail.com>
2018-03-02 15:40:11 +0100

    Ajout pyload et home assistant

--- config.py
+++ config.py
@@ -30,26 +30,33 @@ GEN_MENU={
         'path':u'/',
         'title': u''
         },
-    1: {
+   2: {
         'name': u'T\xe9l\xe9chargements',
         'path': u'SubMenuTitle',
         0: {
             'name':u'Fichiers',
             'path': u'/gestionFichier/mnt/LOC/USB/DDL_OK/',
             'title': u'Gestion des fichiers',
-            'description': u'Permet de d\xe9placer, copier, supprimer des fichiers sur osmc.',
+            'description': u'Permet de d\xe9placer, copier, supprimer des fichiers sur osmc',
             'icone': 'https://png.icons8.com/individual-server/androidL/96'
             },
+      
         1: {
             'name': u'JDownloader',
             'path': u'/JD/',
             'title': u'JDownloader',
             'description': u'Gestion des t\xe9l\xe9chargements (application externe)',
             'icone': 'https://png.icons8.com/jdownloader/androidL/96'
+            },
+        2: {
+            'name': u'Pyload',
+            'path': u'/pyload/',
+            'title': u'Py Load',
+            'description': u'Gestion des t\xe9l\xe9chargements (application externe)',
+            'icone': 'https://png.icons8.com/software-installer/androidL/96'
             }
-   
         },
-    2: {
+    3: {
        'name':u'T\xe9l\xe9commande',
        'path': u'SubMenuTitle',
         0: {
@@ -67,7 +74,7 @@ GEN_MENU={
             'icone': 'https://png.icons8.com/musical-notes/androidL/96'
             }
         },
-    3: {
+    4: {
         'name':u'Etat',
         'path': u'SubMenuTitle',
         0: {
@@ -85,7 +92,7 @@ GEN_MENU={
             'icone': 'https://png.icons8.com/order-history/androidL/96'
             }
         },
-    4: {
+    5: {
         'name':u'Avanc\xe9es',
         'path': u'SubMenuTitle',
         0: {
@@ -106,16 +113,32 @@ GEN_MENU={
             'name': u'Config',
             'path': u'/config/home/osmc/webosmc/config.py',
             'title': u'R\xe9glagles',
-            'description': u'Configuration des param\xe8tres du serveur web osmc.',
+            'description': u'Configuration des param\xe8tres du serveur web osmc',
             'icone': 'https://png.icons8.com/adjust/androidL/96'
             }
 
         },
-    5: {
+    6: {
         'name': u'Cloud',
         'path': u'/ThrowBox/',
         'title': u'Cloud',
-        'description': u'T\xe9l\xe9chargment de fichiers pr\xe9sents sur osmc.',
+        'description': u'T\xe9l\xe9chargment de fichiers pr\xe9sents sur osmc',
         'icone': 'https://png.icons8.com/download-from-the-cloud/androidL/96'
         }
     }
+
+#1: {
+#        'name': u'homeassistant',
+#        'path': u'/homeassistant/',
+#        'title': u'Home Assistant',
+#        'description': u'Interface de gestion de la Domotique pour la maison',
+#        'icone': '/static/images/homeassistant.png'
+#        },
+
+#  1: {
+#            'name': u'pyload',
+#            'path': u'/pyload/',
+#            'title': u'Py Load',
+#            'description': u'Gestion des t\xe9l\xe9chargements (application interne)',
+#            'icone': '/static/images/pyload.png'
+#            },
